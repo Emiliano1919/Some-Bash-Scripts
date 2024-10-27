c768d20a9d3ee426d1d7075540c67503ff0ca588
Emiliano1919 <emilianojg38@gmail.com>
2024-10-26 23:47:11 -0400

    Add coments

--- bubbleSort.sh
+++ bubbleSort.sh
@@ -5,7 +5,9 @@ sort(){
     length=${#array[@]}
     for (( i=1; i < length; i++ )); do
         j=i
-        while [[ j -gt 0 && ${array[j]} < ${array[j-1]} ]]; do
+        while [[ j -gt 0 && ${array[j]} < ${array[j-1]} ]]; do 
+        # The problem was that (()) is only arithmetical we wanted [[]] for conditionals
+        # The previous implementation worked but left a warning.  
             tmp=${array[j]}
             array[j]=${array[j-1]}
             array[j-1]=$tmp
